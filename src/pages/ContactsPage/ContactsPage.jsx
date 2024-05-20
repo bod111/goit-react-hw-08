707bd525f26406efc47d6de4f8a6412636b7d053
bod111 <86015208+bod111@users.noreply.github.com>
2024-05-20 16:15:00 +0300

    7 commit

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import DocumentTitle from "../../components/DocumentTitle/DocumentTitle";
-import { ContactList } from "../../components/ContactList/ContactList";
+import ContactList from "../../components/ContactList/ContactList";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import { fetchContacts } from "../../redux/contacts/operations";
 import { selectIsLoading } from "../../redux/contacts/selectors";
